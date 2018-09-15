d63fb9a554a84cd7137b491f8b7a771d8f922e07
chopchoptr <ivonin148@mail.ru>
2018-09-15 20:25:31 +0300

    Главная

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="style.css">
   <link rel="shortcut icon" href="img/rocket.logo" type="image/x-icon">
   <script type="text/javascript" src="script/jquery-1.12.0.min.js"></script>
+  <script type="text/javascript" src="script/script.js"></script>
 </head>
 <body>
   <header>
@@ -19,13 +20,13 @@
   <div class="vhod">
     <img src="img/logo3.png" alt="">
       <div class="dws-input">
-         <input type="text" name="username" placeholder="Введите имя пользователя" class="log-input">
+         <input type="text" id="login" name="username" placeholder="Введите имя пользователя" class="log-input">
       </div>
       <div class="dws-input">
-         <input type="password" name="password" placeholder="Введите пароль" class="log-input">
+         <input type="password" id="password" name="password" placeholder="Введите пароль" class="log-input">
       </div>
          <div class="dws-submit" name="submit">Войти</div>
-         <div class="dws-submit" name="submit">Регистрация</div> 
+         <div class="dws-submit" name="submit">Регистрация</div>
          <br />
          <a href="#">Забыли пароль?</a>
     </div>
