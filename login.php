bf1067ddd33204b42812aeb136a909804b917df5
chopchoptr <ivonin148@mail.ru>
2018-09-15 15:21:55 +0300

    carousel

--- login.php
+++ login.php
@@ -8,7 +8,6 @@
   <link rel="stylesheet" href="style.css">
   <link rel="shortcut icon" href="img/rocket.logo" type="image/x-icon">
   <script type="text/javascript" src="script/jquery-1.12.0.min.js"></script>
-  <script type="text/javascript" src="script/script.js"></script>
 </head>
 <body>
   <header>
@@ -20,12 +19,13 @@
   <div class="vhod">
     <img src="img/logo3.png" alt="">
       <div class="dws-input">
-         <input type="text" name="username" placeholder="Введите имя пользователя" class="log-input" id="login">
+         <input type="text" name="username" placeholder="Введите имя пользователя" class="log-input">
       </div>
       <div class="dws-input">
-         <input type="password" name="password" placeholder="Введите пароль" class="log-input" id="password">
+         <input type="password" name="password" placeholder="Введите пароль" class="log-input">
       </div>
-         <div class="dws-submit" type="submit" name="submit" style="width: 150px; margin-left: 150px; cursor: pointer;">Войти</div>
+         <div class="dws-submit" name="submit">Войти</div>
+         <div class="dws-submit" name="submit">Регистрация</div> 
          <br />
          <a href="#">Забыли пароль?</a>
     </div>
